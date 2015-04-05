f64e6e726941cfd9ade9b7425d38a7b130362880
tarandeepsingh <tarandeepsingh0910@gmail.com>
2015-04-05 21:06:49 +0530

    yogesh please check this

--- HomeAutomation1/src/com/example/homeautomation/AddDevice.java
+++ HomeAutomation1/src/com/example/homeautomation/AddDevice.java
@@ -1,44 +1,42 @@
 package com.example.homeautomation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class AddDevice extends Activity {
-Button bac,sub;
-    @Override
+	
+
+
+@Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.add);
-        bac = (Button)findViewById(R.id.imageButton2);
-        
-        bac.setOnClickListener(new OnClickListener() {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.add);
+    
+	getActionBar().setDisplayHomeAsUpEnabled(true);   
+    
+  
+    
+     
+	Intent intent = new Intent(getApplicationContext(),Dashboard.class);
 
-    		@Override
-    		public void onClick(View v) {
-    			// TODO Auto-generated method stub
-    			Intent i = new Intent(getBaseContext(),Dashboard.class);
-    			startActivity(i);
-    		}
-        	
-        });
-        sub = (Button)findViewById(R.id.imageButton2);
-        
-        sub.setOnClickListener(new OnClickListener() {
-
-    		@Override
-    		public void onClick(View v) {
-    			// TODO Auto-generated method stub
-    			Intent i = new Intent(getBaseContext(),Dashboard.class);
-    			startActivity(i);
-    		}
-        	
-        });
-        
-    }
-
+   
+	EditText editText = (EditText) findViewById(R.id.editText2);
+	String message = editText.getText().toString();
+	intent.putExtra("txtData", message);
+}    
 }
+
+
+
--- HomeAutomation1/src/com/example/homeautomation/Dashboard.java
+++ HomeAutomation1/src/com/example/homeautomation/Dashboard.java
@@ -1,57 +1,91 @@
 package com.example.homeautomation;
 
-import com.getbase.floatingactionbutton.FloatingActionButton;
-
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import java.util.List;
+import android.widget.Toast;
+import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.Menu;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class Dashboard extends Activity {
 
-	Button  log;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard);
-    
-        final FloatingActionButton add = (FloatingActionButton) findViewById(R.id.add_app);
-        add.setOnClickListener(new OnClickListener() {
-          @Override
-          public void onClick(View v) {
-  			// TODO Auto-generated method stub
-  			Intent i = new Intent(getBaseContext(),AddDevice.class);
-  			startActivity(i);
-  		}
-        });
-        
-        final FloatingActionButton rem1 = (FloatingActionButton) findViewById(R.id.rem_app);
-        rem1.setOnClickListener(new OnClickListener() {
-          @Override
-          public void onClick(View v) {
-  			// TODO Auto-generated method stub
-  			Intent i = new Intent(getBaseContext(),Remove.class);
-  			startActivity(i);
-  		}
-        });
-        
-        log = (Button)findViewById(R.id.button2);
- 
-        log.setOnClickListener(new OnClickListener() {
+    Button add1;
+	public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+	private ListView lv;
+	String message;
 
-    		@Override
-    		public void onClick(View v) {
-    			// TODO Auto-generated method stub
-    			Intent i = new Intent(getBaseContext(),MainActivity.class);
-    			startActivity(i);
-    		}
-        	
-        });
     
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.dashboard);
+	  
+	  try{    
+		    
+			Intent intent = getIntent();
+			 message = intent.getExtras().getString("txtData","");    
+		    }
+		    catch (Exception e) {
+		       
+		        ErrorDialog(e.getMessage());
+		    }
+
+	lv = (ListView) findViewById(R.id.listView1);
+
+     
+    List<String> your_array_list = new ArrayList<String>();
+    your_array_list.add(message);
+        
+
+        // This is the array adapter, it takes the context of the activity as a 
+        // first parameter, the type of list view as a second parameter and your 
+        // array as a third parameter.
+    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>( this,  R.layout.list, your_array_list );
+
+    lv.setAdapter(arrayAdapter); 
+   
+
+    
+    add1 = (Button)findViewById(R.id.button1); 
+    add1.setOnClickListener(new View.OnClickListener() {
+
+     		@Override
+    public void onClick(View v) {
+     			// TODO Auto-generated method stub
+    Intent i = new Intent(getApplicationContext(),AddDevice.class);
+    startActivity(i);
+     		}
+         	
+         });
+     
+	}
+	  
+	  private void ErrorDialog(String Description) {
+	        AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+	                Dashboard.this);
+	        alertDialog.setTitle("You get Error...");
+	        alertDialog.setMessage(Description);
+	        alertDialog.setIcon(R.drawable.dialog_icon);
+
+	        alertDialog.setNegativeButton("Cancel",
+	                new DialogInterface.OnClickListener() {
+	                    public void onClick(DialogInterface dialog, int which) {
+	                        dialog.cancel();
+	                    }
+	                });
+
+	        alertDialog.show();
+	    }
 
 }
+	  
